a4bfb4390554647fffddb01a12ebc3fe6a4649c9
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-09-01 13:28:14 +0300

    finds anagrams of a given word from a provide list

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,19 @@
 # your code goes here!
+class Anagram:
+    # word_received represents the word for which we want to find its anagrams
+    def __init__(self, word_received):
+        # will return the sorted word in characters arranged by alphabetic order
+        self.word = "".join(sorted(word_received))
+
+    def match(self, words_list):
+        # this will hold the anagrams of the word_received form the words_list provided
+        matches = []
+        # checks each word in the given list to see which matches the word_received
+        for word in words_list:
+            if "".join(sorted(word)) == self.word:
+                matches.append(word)
+
+        return matches    
+
+listen = Anagram("listen")
+print(listen.match(["enlists", "goole", 'inlets']))            
